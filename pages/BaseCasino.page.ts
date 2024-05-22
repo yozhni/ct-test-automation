1d1f074fe8dd95abe2b4c7945ba96e5e945dda24
yozhni <85211615+yozhni@users.noreply.github.com>
2024-05-22 18:03:34 +0800

    PR 1, basic setup and script

---
+++ pages/BaseCasino.page.ts
@@ -0,0 +1,41 @@
+import {Locator, Page, expect, test} from "@playwright/test"
+
+export class BaseCasinoPage{ 
+
+    private readonly page: Page;
+
+    constructor(page:Page) { 
+        this.page = page;
+    }
+
+//locators 
+    protected getLocatorPopupFilterButton():Locator {
+        return this.page.locator("xpath=//button[@id='filters-button' and contains(@class, 'filters-button')]");
+    }
+
+    protected getLocatorPopupFilterPageVisible(): Locator { 
+        return this.page.locator("xpath=//div[contains(@class,'filters-overlay') and contains(@class,'translate-x-0')]");
+    }
+
+        protected getLocatorPopupFilterPageNotVisible(): Locator { 
+        return this.page.locator("xpath=//div[contains(@class,'filters-overlay') and contains(@class,'translate-x-full')]");
+    }
+
+//methods
+    async openCasinoBaseURL() { 
+        //await this.page.goto('https://www.cloudbet.com/beta/en/casino/lobby');
+        await this.page.goto('/beta/casino/lobby');
+    }
+
+    async openPopupFilterPage() { 
+        await this.getLocatorPopupFilterButton().click();
+
+    }
+
+    async expectPopupFiltersPageIsVisible() { 
+        await expect(this.getLocatorPopupFilterPageVisible()).toBeVisible();
+    }
+
+    async expectPopupFiltersPageIsHidden() { 
+    }
+}
---
+++ tests/ui/casinoMainPage.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from "@playwright/test"
+import {BaseCasinoPage} from "../../pages/BaseCasino.page"
+
+
+test('Verify the popup Filtes page is opened', async ({ page }) => {
+    const baseCasinoPage = new BaseCasinoPage(page);
+
+    await baseCasinoPage.openCasinoBaseURL();
+    await baseCasinoPage.openPopupFilterPage();
+    await baseCasinoPage.expectPopupFiltersPageIsVisible();
+})
